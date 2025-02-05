e24e2173434f19d383a69a871b5ff1da735c07bd
Perspicacious <johnayomide920@gmail.com>
2025-02-05 17:10:10 +0100

    type error 6

--- app/[shortcode]/page.tsx
+++ app/[shortcode]/page.tsx
@@ -2,27 +2,24 @@ import prisma from '@/lib/db';
 import { redirect } from 'next/navigation';
 import React from 'react'
 
-interface RedirectPageProps {
+type RedirectPageProps = {
     params: { shortcode: string };
-  
-}
-export default async function RedirectPage({ params }: RedirectPageProps) {
+};
 
-    const { shortcode } = params;
+export default async function RedirectPage({ params }: RedirectPageProps) {
+    const { shortcode } = params; // No need to await here
 
     const url = await prisma.url.findUnique({
-        where: {shortCode: shortcode}
-    })
+        where: { shortCode: shortcode }
+    });
 
-    if(!url) {
-        return <div>404 - URL not found</div>
+    if (!url) {
+        return <div>404 - URL not found</div>;
     }
 
     await prisma.url.update({
-        where: {
-            id: url.id
-        },
-        data: {visits: { increment: 1 } },
+        where: { id: url.id },
+        data: { visits: { increment: 1 } },
     });
 
     redirect(url.originalUrl);
