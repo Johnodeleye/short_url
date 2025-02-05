f9cde06325be4d9efa2494122f4295955aac07f8
Perspicacious <johnayomide920@gmail.com>
2025-02-05 20:43:52 +0100

    neon33

--- app/[shortcode]/page.tsx
+++ app/[shortcode]/page.tsx
@@ -29,22 +29,31 @@
 // }
 
 
+
 import prisma from '@/lib/db';
 import { redirect, notFound } from 'next/navigation';
+
 interface RedirectPageProps {
   params: { shortcode: string };
 }
-export default async function RedirectPage({ params }: RedirectPageProps) {
+
+export default async function Page({ params }: RedirectPageProps) {
   const { shortcode } = params; 
+
   const url = await prisma.url.findUnique({
-    where: { shortCode: shortcode },
+    where: { shortCode: shortcode }, 
   });
+  
+  
+
   if (!url) {
     notFound();
   }
+
   await prisma.url.update({
     where: { id: url.id },
     data: { visits: { increment: 1 } },
   });
+
   redirect(url.originalUrl);
 }
