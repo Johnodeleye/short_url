ea4d4ddc1cfd75b7895a8076a22ba7dd18d460c8
Perspicacious <johnayomide920@gmail.com>
2025-02-05 07:56:34 +0100

    front end finidh

---
+++ components/ui/url-list.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import React from 'react'
+import { Button } from './button'
+import { CopyIcon, EyeIcon } from 'lucide-react'
+
+export default function UrlList() {
+  return (
+    <div   >
+      <h2 className="text-2xl font-bold mb-2">Recent URLs</h2>
+      <ul className='space-y-2'>
+        <li className='flex items-center gap-2 justify-between'>
+          <Link 
+          href="https://perspicaciousdev.vercel.app/" className='text-blue-500'
+          target='_blank'
+          >
+            https://perspicaciousdev.vercel.app/
+          </Link>
+       <div className='flex items-center gap-3'>
+        <Button variant='ghost' size='icon' className='text-muted-foreground hover:bg-muted'>
+            <CopyIcon className='w-4 h-4'/>
+            <span className="sr-only">Copy URL</span>
+        </Button>
+        <span className="flex items-center">
+          < EyeIcon className='h-4 w-4'/>
+          100 views
+        </span>
+       </div>
+       </li>
+      </ul>
+    </div>
+  )
+}
