5c313e4ff4fee9721148d5cabd228858fde7f879
Perspicacious <johnayomide920@gmail.com>
2025-02-05 16:15:20 +0100

    done

--- app/[shortcode]/page.tsx
+++ app/[shortcode]/page.tsx
@@ -1,28 +1,21 @@
 import prisma from '@/lib/db';
 import { redirect } from 'next/navigation';
-import React from 'react'
+import { notFound } from 'next/navigation';
 
-interface RedirectPageProps {
-    params: { shortcode: string };
-  
-}
-export default async function RedirectPage({ params }: RedirectPageProps) {
-
-    const { shortcode } = params
+export default async function RedirectPage({ params }: { params: { shortcode: string } }) {
+    const { shortcode } = params;
 
     const url = await prisma.url.findUnique({
-        where: {shortCode: shortcode}
-    })
+        where: { shortCode: shortcode }
+    });
 
-    if(!url) {
-        return <div>404 - URL not found</div>
+    if (!url) {
+        return notFound();
     }
 
     await prisma.url.update({
-        where: {
-            id: url.id
-        },
-        data: {visits: { increment: 1 } },
+        where: { id: url.id },
+        data: { visits: { increment: 1 } },
     });
 
     redirect(url.originalUrl);
